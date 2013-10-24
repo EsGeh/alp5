6163b8de8050203727a476ba2d5881913b9ee296
andreroehrig <andre.roehrig@gmail.com>
2013-10-24 20:39:32 +0200

    aufgabe3 begonnen

---
+++ aufgabe3/src/Fork.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: andre
+ * Date: 10/24/13
+ * Time: 7:31 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Fork {
+    public static Process fork(String input) throws IOException {
+        Process p = Runtime.getRuntime().exec("date");
+        return p;
+    }
+
+    public boolean isremote(String input){
+        if(input.contains(":")){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
---
+++ aufgabe3/src/Main.java
@@ -0,0 +1,27 @@
+import java.io.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: andre
+ * Date: 10/24/13
+ * Time: 7:22 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+    public static void main(String[] args){
+        Process p = null;
+        try {
+            p = Fork.fork("asdf");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        InputStream in = p.getInputStream();
+        BufferedReader bin = new BufferedReader(new InputStreamReader(in));
+        try {
+            System.out.println(bin.readLine());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
