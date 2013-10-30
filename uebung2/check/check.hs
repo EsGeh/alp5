d110d44b40334e88f781c52c8133959e56f0ceca
EsGeh <SamuelGfroerer@googlemail.com>
2013-10-30 02:25:31 +0100

    basic functionality is there

--- uebung2/check/check.hs
+++ uebung2/check/check.hs
@@ -1,9 +1,12 @@
 module Main where
 
+import System.IO
 import System.Environment
 import System.Exit
+import System.Directory(doesFileExist)
 import System.Cmd
 import Text.Regex
+import Control.Monad
 --import Data.List.Split
 
 -- | destination for the local copies of the text and dictionary
@@ -11,24 +14,59 @@ localTextFile = "/tmp/text"
 localDictFile = "/tmp/dict"
 
 main = do
+	hSetBuffering stdout LineBuffering
 	programParams <- (getArgs >>= (return . calcProgramParams))
+	putStrLn "fetching Files..."
 	-- copy remote files onto this machine:
 	fetchFiles (fetchParams programParams)
-	
+	-- # TODO: add error handling
+
+	putStrLn "processing text..."
 	text <- textFromFile localTextFile
 	dict <- dictFromFile localDictFile
 	let range = checkParams programParams
 
- 	case check range dict text of
-		True -> return $ exitSuccess
-		False -> return $ exitFailure
+	let textIsValid = check range dict text 
+	--putStrLn $ show $ textIsValid
 
-check range dict text = and $ map (findWordInDic (lines dict)) $ splitRegex separators text
+	case textIsValid of
+		CheckResult [] -> putStrLn "ok"
+		CheckResult words -> mapM_ print words
+
+
+data CheckResult = CheckResult {
+	getWordList :: [Word]
+}
+type Word = String
+
+check range dict text = --splitAtNewLine dict
+	check' (splitAtNewLine dict) $ splitRegex separators text
 	where
 		-- this regEx should match one ore more occurences of any character, that is not a letter or a number
 		separators = mkRegex "[^[:alnum:]]+"
+		dictSeparator = mkRegex "$"
 
-findWordInDic dict word = elem word dict
+-- | this is a bit complicated, because a line ending can be one of "\r\n" "\n" or "\r"
+splitAtNewLine string = 
+	let (firstLine, restText) = break isLineEnding string
+	in
+		firstLine : case restText of
+			'\r':'\n':rest -> splitAtNewLine rest
+			'\n':rest -> splitAtNewLine rest
+			'\r':rest -> splitAtNewLine rest
+			_ -> []
+	where
+		isLineEnding c = (c =='\r') || (c == '\n')
+
+check' dict [] = CheckResult $ []
+check' dict (firstWord:restWords) = case findWordInDic dict firstWord of
+	Nothing -> check' dict restWords
+	Just word -> CheckResult $ word : (getWordList $ check' dict restWords)
+
+findWordInDic dict word = case elem word dict of
+	True -> Nothing
+	_ -> Just word
+
 
 textFromFile textFile = readFile textFile
 dictFromFile dictFile = readFile dictFile
@@ -52,8 +90,33 @@ calcProgramParams args = case args of
 -- | copy files from the server, and copy it to
 --   localTextFile and localDictFile
 fetchFiles args = do
-	system $ "rsync -au " ++ scpParam (textFileInfo args) ++ " " ++ localTextFile
-	system $ "rsync -au " ++ scpParam (dictFileInfo args) ++ " " ++ localDictFile
+	textFileExists <- doesFileExist localTextFile 
+	dictFileExists <- doesFileExist localDictFile
+	fetchTextReturn <- if textFileExists then return Nothing else
+		fetchFile (textFileInfo args) localTextFile
+	fetchDictReturn <- if dictFileExists then return Nothing else
+		fetchFile (textFileInfo args) localTextFile
+	return $ FetchFilesReturn {
+		fetchTextReturn = fetchTextReturn,
+		fetchDictReturn = fetchDictReturn
+	}
+
+	--when dictFileExists $ system $ "rsync -au " ++ scpParam (dictFileInfo args) ++ " " ++ localDictFile
+	--return ()
+
+fetchFile fileInfo dest = do
+		exitCodeText <- system $ "rsync -au " ++ scpParam fileInfo ++ " " ++ dest
+		case exitCodeText of
+			ExitSuccess -> return $ Nothing
+			ExitFailure i -> return $ Just $ "unable to fetch file \"" ++ show fileInfo ++ "\""
+
+data FetchFilesReturn = FetchFilesReturn {
+	fetchTextReturn :: FetchFileReturn,
+	fetchDictReturn :: FetchFileReturn
+}
+
+type FetchFileReturn = Maybe ErrorMsg
+type ErrorMsg = String
 
 scpParam fileInfo = case serverInfo fileInfo of
 	Nothing -> path fileInfo
