8245ae3badde28049d9a910739e55165b3ca5f8a
EsGeh <SamuelGfroerer@googlemail.com>
2013-11-08 02:28:54 +0100

    f and fProg working again

--- uebung3/aufgabe2/go/src/f/f.go
+++ uebung3/aufgabe2/go/src/f/f.go
@@ -13,58 +13,139 @@ func FSync( digit uint, smaller []uint, greater []uint) (smaller_ , greater_ []u
 
 	smallerIn, greaterIn := make(chan uint), make(chan uint)
 	smallerOut, greaterOut := make(chan uint), make(chan uint)
-	stopF := make(chan Stop)
+
+	eatAllL, eatAllR := make(chan Stop), make(chan Stop)
 
 	go F(
 		digit,
 		smallerIn, greaterIn,
-		smallerOut, greaterOut,
-		stopF )
+		smallerOut, greaterOut )
+
 	// feed F:
+	smallerIn <- uint( len(smaller) )
+	greaterIn <- uint( len(greater) )
+	fmt.Println("starting to feed:")
 	go func() {
-		//fmt.Println("starting to feed:")
 		for i:=0; i<len(smaller); i++ {
-			//fmt.Println("sending", smaller[i])
+			fmt.Println("sending", smaller[i])
 			smallerIn <- smaller[i]
 		}
+	} ()
+	go func() {
 		for i:=0; i<len(greater); i++ {
-			//fmt.Println("sending", greater[i])
+			fmt.Println("sending", greater[i])
 			greaterIn <- greater[i]
 		}
 	} ()
 	// eat from F:
-	for i:=0; i<len(smaller)+len(greater); i++ {
-		select {
-			case e:= <- smallerOut:
-				smaller_ = append(smaller_, e)
-			case e:= <- greaterOut:
-				greater_ = append(greater_, e)
+
+	fmt.Println("starting to eat:")
+	go func() {
+		count := int( <- smallerOut )
+
+		fmt.Println("countLeft:", count)
+		for i:=0; i<count; i++ {
+		//for {
+			select {
+				case e:= <- smallerOut:
+					smaller_ = append(smaller_, e)
+			}
 		}
-	}
+		eatAllL <- Stop{}
+	} ()
+	go func() {
+		count := int( <- greaterOut )
+		fmt.Println("countRight:", count)
+		for i:=0; i<count; i++ {
+		//for {
+			select {
+				case e:= <- greaterOut:
+					greater_ = append(greater_, e)
+			}
+		}
+		eatAllR <- Stop{}
+	} ()
 	
-	//<- eatAll
-	stopF <- Stop{}
+	<- eatAllL
+	<- eatAllR
 	return
 }
 
 /* this implementation is to be used concurrently, just communicating via channels */
 func F(
+	//countList int,
 	digit uint,
 	smallerIn chan uint, greaterIn chan uint,
-	smallerOut chan uint, greaterOut chan uint,
-	stop chan Stop ) {
+	smallerOut chan uint, greaterOut chan uint ) {
+
 	stopSmaller, stopGreater := make(chan Stop), make(chan Stop)
+	stopEatL, stopEatR := make( chan Stop ), make( chan Stop )
+
+	countSmallerIn, countGreaterIn := <- smallerIn, <- greaterIn
+	/*fmt.Println("countSmallerIn",countSmallerIn)
+	fmt.Println("countGreaterIn",countGreaterIn)*/
+
+	sortLSmallerOut, sortLGreaterOut := make( chan uint ), make( chan uint )
+	sortRSmallerOut, sortRGreaterOut := make( chan uint ), make( chan uint )
+
+	var a, b, c, d []uint //:= make([]uint), make([]uint), make([]uint), make([]uint)
+
 	go sort(
 		digit,
 		smallerIn,
-		smallerOut, greaterOut,
+		sortLSmallerOut, sortLGreaterOut,
 		stopSmaller )
 	go sort(
 		digit,
 		greaterIn,
-		smallerOut, greaterOut,
+		sortRSmallerOut, sortRGreaterOut,
 		stopGreater )
-	<- stop
+
+	// eat from sort left:
+	go func() {
+		fmt.Println("starting to eat from left:")
+		for i:=0; i<int(countSmallerIn); i++{
+			fmt.Println("left!")
+			select {
+				case e := <- sortLSmallerOut:
+					a = append( a, e )
+				case e := <- sortLGreaterOut:
+					b = append( b, e )
+			}
+		}
+		fmt.Println("stopping to eat from left:")
+		stopEatL <- Stop{}
+	} ()
+	// eat from sort right:
+	go func() {
+		fmt.Println("starting to eat from right:")
+		for i:=0; i<int(countGreaterIn); i++{
+			fmt.Println("left!")
+			select {
+				case e := <- sortRSmallerOut:
+					c = append( c, e )
+				case e := <- sortRGreaterOut:
+					d = append( d, e )
+			}
+		}
+		fmt.Println("stopping to eat from right:")
+		stopEatR <- Stop{}
+	} ()
+
+	<- stopEatL
+	<- stopEatR
+	fmt.Println("Holla")
+	a = append( a, c... )
+	smallerOut <- uint( len(a) )
+	for i:=0; i<len(a); i++ {
+		smallerOut <- a[i]
+	}
+	b = append( b, d... )
+	greaterOut <- uint( len(b) )
+	for i:=0; i<len(b); i++ {
+		greaterOut <- b[i]
+	}
+	
 	stopSmaller <- Stop{}
 	stopGreater <- Stop{}
 }
